fa323cf0cdf6072f175af928c28e39084e852989
Leslie Middleton <lesliemddltn@gmail.com>
2025-08-27 11:32:19 -0400

    financial_researcher: env + DeepSeek via OpenRouter config; misc tweaks

--- 3_crew/debate/src/debate/main.py
+++ 3_crew/debate/src/debate/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import sys
 import warnings
 
@@ -7,6 +8,19 @@ from datetime import datetime
 from debate.crew import Debate
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except Exception:
+    pass
+
+# Map OpenRouter env to OpenAI-compatible vars if present
+if os.getenv('OPENROUTER_API_KEY') and not os.getenv('OPENAI_API_KEY'):
+    os.environ['OPENAI_API_KEY'] = os.getenv('OPENROUTER_API_KEY')
+
+if os.getenv('OPENROUTER_API_KEY') and not os.getenv('OPENAI_BASE_URL'):
+    os.environ['OPENAI_BASE_URL'] = 'https://openrouter.ai/api/v1'
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
